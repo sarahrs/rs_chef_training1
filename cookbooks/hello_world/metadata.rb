01d8839bc6cc92ca7645831d5b8a08c246c89aa0
sarahrs <sarah.lowe@rightscale.com>
2012-01-25 22:06:46 +0000

    Added hello_world cookbook

---
+++ cookbooks/hello_world/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale"
+maintainer_email "sarah.lowe@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures hello_world"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "hello_world::default", "Prints to the audit entry"
